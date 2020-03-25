466a9370a31863e01da40904bd363e1c9fd26b95
Daniel Norment <danielnorment@gmail.com>
2020-03-24 19:43:44 -0700

    Get info from .blend file, create random positions

--- main.py
+++ main.py
@@ -8,8 +8,6 @@ BLENDER_PATH = "C:/Program Files/Blender Foundation/Blender 2.82/blender.exe"
 SCENES_PATH = "D:/CS/rsolp-blender/input/"
 
 scenes = [f[:-6] for f in os.listdir(SCENES_PATH) if f.endswith('.blend')]   #all names of .blend files in scene folder
-print(scenes)
-
 
 for scene in scenes:
     #Save the size of the plane as a JSON file for limiting input locations
@@ -18,3 +16,25 @@ for scene in scenes:
                     SCENES_PATH + scene + ".blend",
                     "--python",
                     ROOT_PATH + "get_plane_bounds.py"])
+    
+    num = int(input("Number of locations to randomly generate: "))
+
+    #Read in the bounds of the plane
+    bounds = {}
+    with open("./input/" + scene + ".json", "r") as boundfile:
+        bounds = json.load(boundfile)
+        boundfile.close()
+    
+    #Create folder to store inputs
+    os.makedirs("./input/" + scene)
+
+    #Write many JSON files with random x/y in bounds
+    for f in range(num):
+        loc = {
+            "x" : random.uniform(bounds['xmin'], bounds['xmax']),
+            "y" : random.uniform(bounds['ymin'], bounds['ymax'])
+        }
+        with open("./input/" + scene + "/" + str(f) + ".json", "w") as outfile:
+                outfile.write(json.dumps(loc, indent=4))
+                outfile.close()
+
