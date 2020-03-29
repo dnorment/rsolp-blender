118b1f4db7a8b2bfd1fe8a95e5c690baf72e5e8f
Daniel Norment <danielnorment@gmail.com>
2020-03-29 11:46:02 -0700

    Generating input JSON for all scenes

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ import json
 import os
 import random
 import subprocess
+import uuid
 
 ROOT_PATH = "D:/CS/rsolp-blender/"
 BLENDER_PATH = "C:/Program Files/Blender Foundation/Blender 2.82/blender.exe"
 SCENES_PATH = "D:/CS/rsolp-blender/input/"
 
-scenes = [f[:-6] for f in os.listdir(SCENES_PATH) if f.endswith('.blend')]   #all names of .blend files in scene folder
+#Get list of all scenes saved as .blend in /input
+scenes = [f[:-6] for f in os.listdir(SCENES_PATH) if f.endswith('.blend')]
 
 for scene in scenes:
     #Save the size of the plane as a JSON file for limiting input locations
@@ -16,25 +18,41 @@ for scene in scenes:
                     SCENES_PATH + scene + ".blend",
                     "--python",
                     ROOT_PATH + "get_plane_bounds.py"])
-    
-    num = int(input("Number of locations to randomly generate: "))
 
+for scene in scenes:
     #Read in the bounds of the plane
     bounds = {}
     with open("./input/" + scene + ".json", "r") as boundfile:
         bounds = json.load(boundfile)
         boundfile.close()
     
+    #
+    print("Scene: " + scene)
+    model_name = input("Enter model name: ")
+    num = int(input("Number of locations to randomly generate: "))
+
+    
     #Create folder to store inputs
-    os.makedirs("./input/" + scene)
+    try:
+        os.makedirs("./input/" + scene)
+    except FileExistsError:
+        pass
 
     #Write many JSON files with random x/y in bounds
     for f in range(num):
         loc = {
             "x" : random.uniform(bounds['xmin'], bounds['xmax']),
-            "y" : random.uniform(bounds['ymin'], bounds['ymax'])
+            "y" : random.uniform(bounds['ymin'], bounds['ymax']),
+            "model" : model_name
         }
-        with open("./input/" + scene + "/" + str(f) + ".json", "w") as outfile:
+        with open("./input/" + scene + "/" + scene + "-" + model_name + "-" + uuid.uuid4().hex + ".json", "w") as outfile:
                 outfile.write(json.dumps(loc, indent=4))
                 outfile.close()
+
+#Run through input and get list of absolute paths for each input JSON
+input_files = []
+for root, dirs, files in os.walk("./input"):
+    for file in files:
+        if file.endswith('.json') and root != "./input":
+            input_files.append(os.path.abspath(os.path.join(root, file)))
 
